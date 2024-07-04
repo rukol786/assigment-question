83647d1514d8187d00c0697c1d72b7dcf8fb8729
Sana <rukol786786@gmail.com>
2024-07-04 12:11:09 -0700

    Add files via upload

---
+++ sc.js
@@ -0,0 +1,40 @@
+// // // let num1=3;
+// // // let num2=-7;
+// // // let num3=2;
+// // // let product=num1*num2*num3;
+// // // if (product>=4){
+// // //     alert("the sign is +");
+// // // } else{
+// // //     alert("the sign is -");
+// // // }
+// // let num1=0;
+// // let num2=-1;
+// // let num3=4;
+// //    let sorted=[num1,num2,num3]
+// //    sort((num1,num2)؛
+   
+// //     if(num1>num2){
+//         return-1;
+//     }else if(num1<num2){
+//         return 1;
+//     }else{
+//         return 0;
+//     });
+//     alert({sorted[0]},{sorted[1]},{sorted[2]});
+   for(let i= 0 ; i<15;i++){
+    if(i%2===0){  
+      console.log("i is even ");  
+      
+     } else{
+    console.log("i is odd");
+//      }
+    
+function findLargest (a,b){
+    if(a>b){
+        alert(a +"is the largest integer.");
+    }else{
+        alert(b +"is the largest integer. ");
+    }
+}
+
+    
